12cb77c4f2e0b9408f22ba5cd7f9f05c911aa29a
아이슥 <leesak@aiseug-ui-MacBookPro.local>
2020-11-11 17:33:18 +0900

    add Profile Page
    프로필 페이지 구현
    더미데이터로 테스트

--- prepare/front/.next/static/chunks/webpack.js
+++ prepare/front/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1f30cde7b79896325bd9";
+/******/ 	var hotCurrentHash = "20e0855b22235fef2c2d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,15 +1,34 @@
 import React from "react";
 import Head from "next/head";
 import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
+
 const Profile = () => {
+  const followerList = [
+    { nickname: "이삭" },
+    { nickname: "김삭" },
+    { nickname: "박삭" },
+  ];
+  const followingList = [
+    { nickname: "아삭" },
+    { nickname: "바삭" },
+    { nickname: "사각" },
+  ];
   return (
     <>
       <Head>
         <title>내 프로필 | NodeBird</title>
       </Head>
-      <AppLayout>내 프로필</AppLayout>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
     </>
   );
 };
 
 export default Profile;
+
+//먼저 큼직큼직하게 가상의 컴포넌트를 만들자
